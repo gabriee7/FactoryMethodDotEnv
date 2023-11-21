f1519e6bd03ea465f7f433364e32ec1da6c519c1
gabriee7 <gabrielsouzades@gmail.com>
2023-11-21 16:35:48 -0300

    diagrama de classe

---
+++ FactoryMethod/src/main/java/com/mycompany/factorymethod/FactoryMethod.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.factorymethod;
+
+/**
+ *
+ * @author nitro5
+ */
+public class FactoryMethod {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
